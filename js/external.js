9c626b92de03ae44cbb5c325d668772597cff9ff
lancecrowder <thelancecrowder@gmail.com>
2018-03-19 11:17:35 -0500

    JS and HTML internal and external exercises

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict"
+
+alert("Welcome to my website!");
+
+var favoriteColor = prompt("Buddy Elf! Whats your favorite color?");
+
+alert("Great " + favoriteColor + " is my favorite color too!!!");
+
+console.log("Hello from external JavaScript");
