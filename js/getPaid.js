9a5e1044e644ad04faa300a46bc71734410303df
lancecrowder <thelancecrowder@gmail.com>
2018-03-19 11:54:58 -0500

    Getting paid Excercise

---
+++ js/getPaid.js
@@ -0,0 +1,7 @@
+var googleDays = prompt("How many days did you work for Google?");
+var amazonDays = prompt("How many days did you work for Amazon?");
+var facebookDays = prompt("How many days did you work for Facebook?")
+
+var total = ((googleDays * 400)+(amazonDays*380)+(facebookDays*350))
+
+alert("You've made $"+total)
