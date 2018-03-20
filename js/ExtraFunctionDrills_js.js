c79c34fc2339a16d33d2d717e9db5c762ca22a9f
lancecrowder <thelancecrowder@gmail.com>
2018-03-20 11:20:45 -0500

    isEven complete

--- js/ExtraFunctionDrills_js.js
+++ js/ExtraFunctionDrills_js.js
@@ -1,23 +1,37 @@
 //Write a function named identity that defines one parameter and returns the argument it was called with.
 
-var oneParameter = "Lance";
-
-function identity(oneParameter) {
-    return (oneParameter)
-}
-
-console.log(identity(oneParameter));
+// var oneParameter = "Lance";
+//
+// function identity(oneParameter) {
+//     return (oneParameter)
+// }
+//
+// console.log(identity(oneParameter));
 
 //Write a function named isOdd that accepts a number and returns true if the passed number is odd, otherwise false
 
+// var myNumber = prompt("Please, enter a number.");
+//
+// function isOdd(myNumber) {
+//     if (myNumber % 2 === 0){
+//         return false
+//     } else {
+//         return true
+//     }
+// }
+//
+// alert("Your number is odd:  " + isOdd(myNumber));
+
+//Write a function named isEven that accepts a number and returns true if the passed number is even, false otherwise
+
 var myNumber = prompt("Please, enter a number.");
 
-function isOdd(myNumber) {
+function isEven(myNumber) {
     if (myNumber % 2 === 0){
-        return false
-    } else {
         return true
+    } else {
+        return false
     }
 }
 
-alert("Your number is odd:  " + isOdd(myNumber));
+alert("Your number is even:  " + isEven(myNumber));
