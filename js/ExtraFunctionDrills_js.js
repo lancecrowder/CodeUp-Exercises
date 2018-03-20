6c61fa558860309098908eff920b0c59ed70f748
lancecrowder <thelancecrowder@gmail.com>
2018-03-20 11:24:30 -0500

    isPositive complete

--- js/ExtraFunctionDrills_js.js
+++ js/ExtraFunctionDrills_js.js
@@ -24,14 +24,28 @@
 
 //Write a function named isEven that accepts a number and returns true if the passed number is even, false otherwise
 
+// var myNumber = prompt("Please, enter a number.");
+//
+// function isEven(myNumber) {
+//     if (myNumber % 2 === 0){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+//
+// alert("Your number is even:  " + isEven(myNumber));
+
+//Write a function named isPositive that returns true if the passed number is greater than 0
+
 var myNumber = prompt("Please, enter a number.");
 
-function isEven(myNumber) {
-    if (myNumber % 2 === 0){
+function isPositive(myNumber) {
+    if (myNumber > 0){
         return true
     } else {
         return false
     }
 }
 
-alert("Your number is even:  " + isEven(myNumber));
+alert("Your number is Positive:  " + isPositive(myNumber));
