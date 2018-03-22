153187c62e8fa9afe0a18a0844756a2828e40615
lancecrowder <thelancecrowder@gmail.com>
2018-03-22 16:58:05 -0500

    EOD Push

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,15 +10,20 @@
 // showMultiplicationTable(anyNumber);
 
 
-function isOdd(randomNumber) {
-    if (randomNumber % 2 === 0) {
-        return " is even";
-    } else {
-        return " is odd";
-    }
-}
+// function isOdd(randomNumber) {
+//     if (randomNumber % 2 === 0) {
+//         return " is even";
+//     } else {
+//         return " is odd";
+//     }
+// }
+//
+// for (var i=1; i<=10; i++) {
+//     var randomNumber = Math.floor(Math.random() * 180) + 20;
+//     console.log(randomNumber + isOdd(randomNumber));
+// }
 
-for (i=1; i<=10; i++) {
-    var randomNumber = Math.floor(Math.random() * 180) + 20;
-    console.log(randomNumber + isOdd(randomNumber));
+for (var i=1; i<10; i++) {
+    // parseFloat(i);
+    console.log(i.repeat(i));
 }
