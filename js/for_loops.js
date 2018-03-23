7cc2839f3e938601df8b2d03c5a12d3a7d610114
lancecrowder <thelancecrowder@gmail.com>
2018-03-23 09:34:01 -0500

    Break and Continue completed.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,5 +1,5 @@
-'use strict';
-
+// 'use strict';
+//
 // var anyNumber  = prompt("Pick a Number, any Number!");
 //
 // function showMultiplicationTable(anyNumber){
@@ -10,8 +10,8 @@
 // }
 //
 // showMultiplicationTable(anyNumber);
-
-
+//
+//
 // function isOdd(randomNumber) {
 //     if (randomNumber % 2 === 0) {
 //         return " is even";
@@ -24,13 +24,13 @@
 //     var randomNumber = Math.floor(Math.random() * 180) + 20;
 //     console.log(randomNumber + isOdd(randomNumber));
 // }
-
+//
 // for (var i=1; i<10; i++) {
 //     var aString=i.toString();
 //     console.log(aString.repeat(i));
 // }
-
+//
 // for (var i=100; i>=5; i=i-5) {
 //     console.log(i);
 // }
-
+//
