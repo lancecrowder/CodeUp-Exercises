092b7ba42dbcbcac1d81fc472963a7d5463c2848
lancecrowder <thelancecrowder@gmail.com>
2018-03-23 16:52:58 -0500

    iterating arrays js completed

--- js/iterating.js
+++ js/iterating.js
@@ -7,29 +7,40 @@
      * 'names'.
      */
 
+    var names = ["Lance", "Jennifer", "Thomas", "Logan" ];
+
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
 
+    for (var i=0; i<4; i++){
+        console.log(names[i]);
+    }
+
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+    names.forEach(function (element) {
+        console.log(element)
+    });
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +54,23 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
+    function first(i) {
+        return i = names [0];
+    }
+
+    console.log(first(names));
+
+    function second(i) {
+        return i =names [1];
+    }
+
+    console.log(second(names));
+
+    function last(i) {
+        return i = names [names.length-1];
+    }
+
+    console.log(last(names));
 
 })();
