9b179f6ddac7f84bcaba0af9d501408b1ba2a538
lancecrowder <thelancecrowder@gmail.com>
2018-03-23 10:25:22 -0500

    Break and Continue completed and cleaned up.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,12 +1,14 @@
-var userNumber = prompt("Please, enter a number between 1 and 50.");
-
-for (i=1; i<=50; i++) {
-    if (i==userNumber) {
-        console.log("Yikes! Skipping number: " + i);
-    }
-        else if (i % 2 === 0){
-        continue;
-    } else {
-        console.log("Here is an odd number: " + i);
-    }
-}
+// 'use strict';
+//
+// var userNumber = prompt("Please, enter a number between 1 and 50.");
+//
+// for (i=1; i<=50; i++) {
+//     if (i==userNumber) {
+//         console.log("Yikes! Skipping number: " + i);
+//     }
+//         else if (i % 2 === 0){
+//         continue;
+//     } else {
+//         console.log("Here is an odd number: " + i);
+//     }
+// }
--- js/while.js
+++ js/while.js
@@ -1,3 +1,5 @@
+// 'use strict';
+//
 // var i = 2;
 //
 // while (i<=65536){
