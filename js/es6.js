6e2557f672286df5f7bc0eeb69820145529f02d1
lancecrowder <thelancecrowder@gmail.com>
2018-04-19 11:41:34 -0500

    Completed es6.js

--- js/es6.js
+++ js/es6.js
@@ -67,25 +67,25 @@ users.forEach(user => {
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach(function(user) {
+users.forEach(user => {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+  const user = {name: 'Lance', email: 'thelancecrowder@gmail.com', languages:['html', 'css', 'javascript', 'jQuery']};
+  const {name, email, languages} = user;
+  // const name = user.name;
+  // const email = user.email;
+  // const languages = user.languages;
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+  developers.push(`${name}'s email is ${email} ${name}  knows ${languages.join(', ')}`);
 });
 
 // TODO: Use `let` for the following variable
 let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
-
+// developers.forEach(function (developer) {
+for (let developer of developers)
   // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
-});
-list += '</ul>';
+  list += (`<li>${developer}</li></ul>`);
