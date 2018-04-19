87e2c42ca2bc698c92a088822df1eab017660088
lancecrowder <thelancecrowder@gmail.com>
2018-04-19 13:41:07 -0500

    added html

--- js/es6.js
+++ js/es6.js
@@ -32,6 +32,7 @@ const users = [
 // TODO: fill in your name and email and add some programming languages you know
 // to the languages array
 
+
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
 let name = 'Lance';
