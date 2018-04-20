51e06a5d29b22fe69a7a9a74c6a56250343ff41f
lancecrowder <thelancecrowder@gmail.com>
2018-04-20 14:45:38 -0500

    Wait function completed

--- js/promises.js
+++ js/promises.js
@@ -1,7 +1,12 @@
 "use strict";
-const wait  = new Promise((resolve, reject)=>{
 
-});
-
+function wait(num){
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, num);
+    });
+}
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
