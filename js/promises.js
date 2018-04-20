6a0a0ac1a6e4dfbf2e8e26b2c547bb30ff579dcc
lancecrowder <thelancecrowder@gmail.com>
2018-04-20 16:46:08 -0500

    Completed

--- js/promises.js
+++ js/promises.js
@@ -10,3 +10,12 @@ function wait(num){
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
+// const gitHubToken = 8f006029edb310e5b5d402ce12c66526671acb3f;
+
+function getLastCommit () {
+    fetch('https://api.github.com/users/lancecrowder/events/public', {headers: {'Authorization': 'token 8f006029edb310e5b5d402ce12c66526671acb3f'}})
+        .then(response => response.json())
+        .then((users) => console.log(users[0].created_at));
+}
+
+getLastCommit();
