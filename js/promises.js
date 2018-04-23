43764ae2462c0aff7e6827843bb8f3a479659f92
lancecrowder <thelancecrowder@gmail.com>
2018-04-23 09:59:18 -0500

    Completed with corrected token

--- js/promises.js
+++ js/promises.js
@@ -13,8 +13,9 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 // const gitHubToken = 8f006029edb310e5b5d402ce12c66526671acb3f;
 
 function getLastCommit () {
-    fetch('https://api.github.com/users/lancecrowder/events/public', {headers: {'Authorization': 'token 8f006029edb310e5b5d402ce12c66526671acb3f'}})
+    fetch('https://api.github.com/users/lancecrowder/events/public', {headers: {'Authorization': 'token'}})
         .then(response => response.json())
+        // .then((users) => console.log(users.map(user => user.created_at)))
         .then((users) => console.log(users[0].created_at));
 }
 
