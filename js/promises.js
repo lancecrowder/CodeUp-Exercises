f59dc4ed4a8f944d4641567ae45c483a951d66b1
lancecrowder <thelancecrowder@gmail.com>
2018-04-23 10:23:43 -0500

    Completed without corrected token!

--- js/promises.js
+++ js/promises.js
@@ -10,13 +10,11 @@ function wait(num){
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
-// const gitHubToken = 8f006029edb310e5b5d402ce12c66526671acb3f;
-
 function getLastCommit () {
-    fetch('https://api.github.com/users/lancecrowder/events/public', {headers: {'Authorization': 'token'}})
+    fetch('https://api.github.com/users/lancecrowder/events', {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
         .then(response => response.json())
         // .then((users) => console.log(users.map(user => user.created_at)))
-        .then((users) => console.log(users[0].created_at));
+        .then((users) => console.log(users[0].created_at))
+        .catch(error => console.log(error))
 }
-
 getLastCommit();
